b6f3aa4b8923203961d8a96b4e3ca20f10229e89
largado <alexandrohipolito@gmail.com>
2022-05-27 13:54:43 -0300

    Add project files.

---
+++ Idm/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Idm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string endereco = "https://avena.azurewebsites.net/";
+
+            string resposta = new HttpClient().GetStringAsync(endereco).Result;
+
+            Console.WriteLine(resposta);
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
